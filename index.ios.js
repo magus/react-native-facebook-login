917af3f27747811aa06d66f48ceedb4a2bf622d1
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2015-03-26 21:42:52 -0700

    fetch rottentomatoes movies in theaters and render touchable

--- index.ios.js
+++ index.ios.js
@@ -11,27 +11,82 @@ var {
   Image,
   Text,
   View,
+  TouchableHighlight
 } = React;
+
+var API_KEY = '7waqfqbprs7pajbz28mqf6vz';
+var API_URL = 'http://api.rottentomatoes.com/api/public/v1.0/lists/movies/in_theaters.json';
+var PAGE_SIZE = 25;
+var PARAMS = '?apikey=' + API_KEY + '&page_limit=' + PAGE_SIZE;
+var REQUEST_URL = API_URL + PARAMS;
 
 var MOCKED_MOVIES_DATA = [
   {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
 var toy = React.createClass({
-  render: function() {
-    var movie = MOCKED_MOVIES_DATA[0];
+  getInitialState: function(){
+    return {
+      movies: null,
+    };
+  },
+
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  movieClicked: function(movie){
+    console.log(movie.title, "was clicked");
+  },
+
+  renderLoadingView: function(){
     return (
       <View style={styles.container}>
-        <Image
-          style={styles.thumbnail}
-          source={{uri: movie.posters.thumbnail}}
-        />
-        <View style={styles.rightContainer}>
-          <Text style={styles.title}>{movie.title}</Text>
-          <Text style={styles.year}>{movie.year}</Text>
-        </View>
+        <Text>
+          Loading movies...
+        </Text>
       </View>
     );
+  },
+
+  renderMovie: function(movie){
+    return (
+      <TouchableHighlight
+        onPress={this.movieClicked.bind(this, movie)}
+        style={styles.container}
+      >
+        <View style={styles.container}>
+          <Image
+            style={styles.thumbnail}
+            source={{uri: movie.posters.thumbnail}}
+          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+        </View>
+      </TouchableHighlight>
+    );
+  },
+
+  render: function() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
   }
 });
 
