9c4ef2eca28beae252ce12de7423fe170db437e7
magus <noah@iamnoah.com>
2015-03-29 02:28:12 -0700

    request omdb and imdb for poster and imdb score

--- MovieView.js
+++ MovieView.js
@@ -8,6 +8,7 @@ var {
   StyleSheet,
   Text,
   View,
+  ActivityIndicatorIOS,
 } = React;
 
 var MovieView = React.createClass({
@@ -15,15 +16,94 @@ var MovieView = React.createClass({
     movie: React.PropTypes.object.isRequired,
   },
 
+  getInitialState: function(){
+    return {
+      imdb: "",
+      poster: "",
+    };
+  },
+
+  getIMDBIDOMDB: function(yearAdjust){
+    var _this = this;
+    var movie = this.props.movie;
+    var title = encodeURIComponent(movie.title);
+    var yearAdjust = yearAdjust ? yearAdjust : 0;
+    var year = (parseInt(movie.year)+yearAdjust).toString();
+
+    return fetch(`http://www.omdbapi.com/?type=movie&s=${title}&y=${year}`)
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (_this.isMounted()){
+          var resultsKey = "Search";
+          if (responseData.hasOwnProperty(resultsKey)) {
+            // Call other APIs with IMDB id
+            var imdbID = responseData[resultsKey][0].imdbID;
+            this.getPoster(imdbID);
+            this.getIMDB(imdbID);
+          } else {
+            // Search all movie results within 3 years
+            if (yearAdjust < 3) {
+              // Search forwards then backwards
+              yearAdjust = yearAdjust <= 0 ? yearAdjust + 1 : yearAdjust * -1;
+              // Call self with new yearAdjust
+              _this.getIMDBIDOMDB(yearAdjust);
+            }
+          }
+        }
+      });
+  },
+
+  getIMDB: function(imdbID){
+    var _this = this;
+    var IMDB_API = `http://app.imdb.com/title/maindetails?tconst=${imdbID}`;
+    console.log(IMDB_API);
+
+    return fetch(IMDB_API)
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (_this.isMounted()){
+          _this.setState({ imdb : responseData });
+        }
+      });
+  },
+
+  getPoster: function(imdbID){
+    var _this = this;
+    var OMDB_API = `http://www.omdbapi.com/?i=${imdbID}&plot=short&r=json`;
+    console.log(OMDB_API);
+
+    return fetch(OMDB_API)
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (_this.isMounted()){
+          _this.setState({ poster : responseData.Poster });
+        }
+      });
+  },
+
+  componentDidMount: function () {
+    this.request = this.getIMDBIDOMDB();
+  },
+
   render: function() {
     var movie = this.props.movie;
+    var poster = this.state.poster;
+    var imdb = this.state.imdb;
+    var imdbRating = imdb ? imdb.data.rating : "N/A";
+
+    var posterImage = poster ?
+      <Image
+        source={{uri: poster}}
+        style={styles.detailsImage}
+      />
+      : <View style={styles.detailsImage}>
+        <ActivityIndicatorIOS size={"large"} />
+      </View>;
+
     return (
       <ScrollView contentContainerStyle={styles.contentContainer}>
         <View style={styles.mainSection}>
-          <Image
-            source={{uri: movie.posters.thumbnail}}
-            style={styles.detailsImage}
-          />
+          { posterImage }
           <View style={styles.rightPane}>
             <Text style={styles.movieTitle}>{this.props.movie.title}</Text>
             <Text>{this.props.movie.year}</Text>
@@ -33,6 +113,7 @@ var MovieView = React.createClass({
               </Text>
             </View>
             <Text>{this.props.movie.ratings}</Text>
+            <Text>{imdbRating}</Text>
           </View>
         </View>
         <View style={styles.separator} />
@@ -89,6 +170,10 @@ var styles = StyleSheet.create({
     height: 200,
     backgroundColor: '#eaeaea',
     marginRight: 10,
+
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   separator: {
     backgroundColor: 'rgba(0, 0, 0, 0.1)',
