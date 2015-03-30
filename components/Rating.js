1b82bc99c7319b362338f9d0b65830286f56366c
magus <noah@iamnoah.com>
2015-03-29 21:51:36 -0700

    react global via window/GLOBAL object

--- components/Rating.js
+++ components/Rating.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var React = require('react-native');
 var {
   Text,
 } = React;
--- components/RottenTomatoeRatings.js
+++ components/RottenTomatoeRatings.js
@@ -1,7 +1,5 @@
 'use strict';
 
-var React = require('react-native');
-
 var {
   StyleSheet,
   Text,
