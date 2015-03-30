1a5de10b21b81e392529d233b5a615c081da83cf
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2015-03-29 21:47:58 -0700

    rotten tomatoes rating component

---
+++ components/RottenTomatoeRatings.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var React = require('react-native');
+
+var {
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var Rating = require('./Rating');
+
+var RottenTomatoeRatings = React.createClass({
+  propTypes: {
+    critics: React.PropTypes.number,
+    audience: React.PropTypes.number,
+  },
+
+  render: function() {
+    return (
+      <View style={styles.flexRow}>
+        <Rating score={this.props.critics} />
+        <Text style={styles.ratingSpacer}>{" - "}</Text>
+        <Rating score={this.props.audience} />
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  flexRow: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  ratingSpacer: {
+    marginRight: 5,
+  },
+});
+
+module.exports = RottenTomatoeRatings;
