559043794c79f9ac19bcac5f423151f2ed4ed172
Noah Jorgenson <noah.m.jorgenson@gmail.com>
2015-04-02 02:30:20 -0700

    display user profile picture, email and name

--- components/Login.js
+++ components/Login.js
@@ -2,6 +2,7 @@
 
 var {
   StyleSheet,
+  Image,
   Text,
   View,
   AlertIOS,
@@ -14,10 +15,12 @@ var FBLogin = require('./facebook/FBLogin');
 var FBLoginButton = require('./iOS/FBLoginButton.ios.js')
 var FacebookLogin = require('NativeModules').FacebookLogin;
 
+var FB_PHOTO_WIDTH = 200;
+
 var Login = React.createClass({
   getInitialState: function(){
     return {
-      user: {},
+      user: null,
     };
   },
 
@@ -36,18 +39,107 @@ var Login = React.createClass({
 
   render: function() {
     var user = this.state.user;
-    var creds = user
-      ? <View>
-          <Text>{ user.userId }</Text>
-          <Text>{ user.token }</Text>
-        </View>
-      : <Text>N/A</Text>;
 
     return (
       <View style={styles.loginContainer}>
-        <FBLoginButton />
+        { user && <Photo user={user} /> }
+        { user && <Info user={user} /> }
 
-        { creds }
+        <FBLoginButton style={{ marginBottom: 10, }} />
+
+        <Text>{ user ? user.token : "N/A" }</Text>
+      </View>
+    );
+  }
+});
+
+var Photo = React.createClass({
+  propTypes: {
+    user: React.PropTypes.object.isRequired,
+  },
+
+  getInitialState: function(){
+    return {
+      photo: null,
+    };
+  },
+
+  componentWillMount: function(){
+    var _this = this;
+    var user = this.props.user;
+    var api = `https://graph.facebook.com/v2.3/${user.userId}/picture?width=${FB_PHOTO_WIDTH}&redirect=false&access_token=${user.token}`;
+
+    fetch(api)
+      .then((response) => response.json())
+      .then((responseData) => {
+        _this.setState({
+          photo : {
+            url : responseData.data.url,
+            height: responseData.data.height,
+            width: responseData.data.width,
+          },
+        });
+      })
+      .done();
+  },
+
+  render: function(){
+    var photo = this.state.photo;
+
+    return (
+      <View style={styles.bottomBump}>
+
+        <Image
+          style={photo &&
+            {
+              height: photo.height,
+              width: photo.width,
+            }
+          }
+          source={{uri: photo && photo.url}}
+        />
+      </View>
+    );
+  }
+});
+
+var Info = React.createClass({
+  propTypes: {
+    user: React.PropTypes.object.isRequired,
+  },
+
+  getInitialState: function(){
+    return {
+      info: null,
+    };
+  },
+
+  componentWillMount: function(){
+    var _this = this;
+    var user = this.props.user;
+    var api = `https://graph.facebook.com/v2.3/${user.userId}?fields=name,email&access_token=${user.token}`;
+
+    fetch(api)
+      .then((response) => response.json())
+      .then((responseData) => {
+        _this.setState({
+          info : {
+            name : responseData.name,
+            email: responseData.email,
+          },
+        });
+      })
+      .done();
+  },
+
+  render: function(){
+    var info = this.state.info;
+
+    return (
+      <View style={styles.bottomBump}>
+        <Text>{ info && this.props.user.userId }</Text>
+        <Text>{ info && info.name }</Text>
+        <Text>{ info && info.email }</Text>
       </View>
     );
   }
@@ -55,11 +147,14 @@ var Login = React.createClass({
 
 var styles = StyleSheet.create({
   loginContainer: {
-    marginTop: 375,
+    marginTop: 150,
 
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomBump: {
+    marginBottom: 15,
   },
 });
 
