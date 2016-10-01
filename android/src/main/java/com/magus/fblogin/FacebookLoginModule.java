856cfd671abfd6b9c0a54a0771c16390ba4a89c4
Layton Whiteley <layton.whiteley@gmail.com>
2016-10-01 12:45:39 -0400

    fix(android): add missing import

--- android/src/main/java/com/magus/fblogin/FacebookLoginModule.java
+++ android/src/main/java/com/magus/fblogin/FacebookLoginModule.java
@@ -19,6 +19,7 @@ import com.facebook.login.LoginResult;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
