b903912267fa00d8d805c771ae8c9041f2b5220a
Mehul Mohan <mehulmpt@gmail.com>
2018-09-22 15:43:14 +0530

    Added support for buttonView on iOS
    buttonView renders correctly for iOS now

--- index.ios.js
+++ index.ios.js
@@ -8,8 +8,11 @@ import {
   NativeModules,
   NativeMethodsMixin,
   DeviceEventEmitter,
-  requireNativeComponent
+  requireNativeComponent,
+  TouchableHighlight
 } from 'react-native';
+
+import itypeof from 'itypeof'
 
 const FBLoginManager = NativeModules.MFBLoginManager;
 const RCTMFBLogin = requireNativeComponent('RCTMFBLogin', FBLogin);
@@ -26,6 +29,14 @@ class FBLogin extends Component {
   constructor (props) {
     super(props);
 
+
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this._handleEvent = this._handleEvent.bind(this);
+    this._getButtonView = this._getButtonView.bind(this);
+    this.getChildContext = this.getChildContext.bind(this);
+	this._onFacebookPress = this._onFacebookPress.bind(this);
+	
     this.bindAll = this.bindAll.bind(this);
     this.bindAll();
 
@@ -85,8 +96,91 @@ class FBLogin extends Component {
     });
   }
 
+  //render() {
+  //  return <RCTMFBLogin {...this.props} style={[styles.base, this.props.style]} />
+  //}
+
   render() {
-    return <RCTMFBLogin {...this.props} style={[styles.base, this.props.style]} />
+	  return (<TouchableHighlight onPress={this._onFacebookPress} underlayColor={this.props.onClickColor} >
+        <View style={[this.props.containerStyle]}>
+          {this._getButtonView()}
+        </View>
+      </TouchableHighlight>)
+  }
+
+
+  //custom
+
+  _getButtonView () {
+    const buttonText = this.props.facebookText ? this.props.facebookText:this.state.buttonText;
+    return (this.props.buttonView)
+      ? this.props.buttonView
+      : (
+        <View style={[styles.login, this.props.style]}>
+          <Text style={[styles.whiteFont, this.fontStyle]}> {buttonText} </Text>
+        </View>
+      );
+  }
+
+  login(permissions) {
+    FBLoginManager.loginWithPermissions(
+      permissions || this.props.permissions,
+      (err,data) => this._handleEvent(err,data)
+    );
+  }
+
+  getChildContext () {
+    return {
+      isLoggedIn: this.state.isLoggedIn,
+      login: this.login,
+      logout: this.logout,
+      props: this.props
+    };
+
+  }
+
+  logout() {
+    FBLoginManager.logout((err, data) => this._handleEvent(err, data));
+  }
+
+  _handleEvent(e, data) {
+    const result = e || data;
+    if(result.type === 'success' && result.profile){
+      try{
+        result.profile = JSON.parse(result.profile)
+      } catch (err) {
+        console.warn('Could not parse facebook profile: ', result.profile);
+        console.error(err);
+      }
+    }
+
+    if(result.eventName === 'onLogin' || result.eventName === 'onLoginFound'){
+      this.setState({isLoggedIn:true, buttonText: this.state.statics.logoutText});
+    } else if (result.eventName === 'onLogout'){
+      this.setState({isLoggedIn:false, buttonText: this.state.statics.loginText});
+    }
+
+    if(result.eventName && this.props.hasOwnProperty(result.eventName)){
+      const event = result.eventName;
+      delete result.eventName;
+      console.log('Triggering \'%s\' event', event)
+      this.props[event](result);
+    } else {
+      console.log('\'%s\' Event is not defined or recognized', result.eventName)
+    }
+  }
+
+  _onFacebookPress() {
+    let permissions = [];
+    if( itypeof(this.props.permissions) === 'array'){
+      permissions = this.props.permissions;
+    }
+
+    if(this.state.isLoggedIn){
+      this.logout()
+    }else{
+      this.login(permissions)
+    }
   }
 }
 
